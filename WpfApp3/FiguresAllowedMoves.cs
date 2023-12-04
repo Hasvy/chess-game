0d6df09e304db1bef95e84248381bcc746ed86a6
Hasvy <143729667+Hasvy@users.noreply.github.com>
2023-12-04 13:26:05 +0100

    Add files via upload

---
+++ WpfApp3/FiguresAllowedMoves.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using WpfApp3;
+
+namespace WpfApp3
+{
+    class FiguresAllowedMoves
+    {
+        CellsList cells = MainWindow.cells;
+
+        public FiguresAllowedMoves()
+        {
+            
+        }
+
+        public void Pawn_Allowed_Moves(int row, int col, char color)                          //r, c is starting position, writed when MouseDown
+        {
+            int length = 1;
+            if (row == 1 || row == 6)
+            {
+                length = 2;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                row = (color == 'w') ? row - 1 : row + 1;
+                if (row >= 0 && row <= 7)
+                {
+                    Grid cell;
+                    if (col > 0 && i == 0)
+                    {
+                        cell = cells.Find(row, col - 1).Name;
+                        if (CheckFigureInCell(cell) != 'n' && CheckFigureInCell(cell) != color)
+                        {
+                            LightingCell(cell);
+                        }
+                    }
+                    if (col < 7 && i == 0)
+                    {
+                        cell = cells.Find(row, col + 1).Name;
+                        if (CheckFigureInCell(cell) != 'n' && CheckFigureInCell(cell) != color)
+                        {
+                            LightingCell(cell);
+                        }
+                    }
+                    cell = cells.Find(row, col).Name;
+                    if (CheckFigureInCell(cell) != 'n')
+                    {
+                        break;
+                    }
+                    LightingCell(cell);
+                }
+            }
+        }
+
+        public void King_Allowed_Moves(int row, int col, char color)
+        {
+            Grid cell;
+            int xStart, yStart, xEnd, yEnd;
+            yStart = (row == 7) ? 0 : -1;
+            yEnd = (row == 0) ? 0 : 1;
+            xStart = (col == 0) ? 0 : -1;
+            xEnd = (col == 7) ? 0 : 1;
+            for (int y = yStart; y <= yEnd; y++)
+            {
+                for (int x = xStart; x <= xEnd; x++)
+                {
+                    cell = cells.Find(row - y, col + x).Name;
+                    if (!((y == 0 && x == 0) || (CheckFigureInCell(cell) == color)))
+                    {
+                        LightingCell(cell);
+                    }
+                }
+            }
+        }
+
+        public void Queen_Allowed_Moves(int row, int col, char color)
+        {
+            DirectMoves(row, col, color);
+            DiagonalMoves(row, col, color);
+        }
+
+        internal void Bishop_Allowed_Moves(int row, int col, char color)
+        {
+            DiagonalMoves(row, col, color);
+        }
+        internal void Knight_Allowed_Moves(int row, int col, char color)
+        {
+            if (row < 6)
+            {
+                if (col > 0)
+                {
+                    LightIfNotSameColor(cells.Find(row + 2, col - 1).Name, color);
+                }
+                if (col < 7)
+                {
+                    LightIfNotSameColor(cells.Find(row + 2, col + 1).Name, color);
+                }
+            }
+            if (col > 1)
+            {
+                if (row > 0)
+                {
+                    LightIfNotSameColor(cells.Find(row - 1, col - 2).Name, color);
+                }
+                if (row < 7)
+                {
+                    LightIfNotSameColor(cells.Find(row + 1, col - 2).Name, color);
+                }
+            }
+            if (row > 1)
+            {
+                if (col > 0)
+                {
+                    LightIfNotSameColor(cells.Find(row - 2, col - 1).Name, color);
+                }
+                if (col < 7)
+                {
+                    LightIfNotSameColor(cells.Find(row - 2, col + 1).Name, color);
+                }
+            }
+            if (col < 6)
+            {
+                if (row > 0)
+                {
+                    LightIfNotSameColor(cells.Find(row - 1, col + 2).Name, color);
+                }
+                if (row < 7)
+                {
+                    LightIfNotSameColor(cells.Find(row + 1, col + 2).Name, color);
+                }
+            }
+        }
+
+        internal void Rook_Allowed_Moves(int row, int col, char color)
+        {
+            DirectMoves(row, col, color);
+        }
+
+        private void DirectMoves(int row, int col, char color)
+        {
+            bool up, down, left, right;
+            up = down = left = right = true;
+            for (int i = 1; i <= 7; i++)
+            {
+                if (row + i <= 7 && up)
+                {
+                    up = CheckFigureColor(cells.Find(row + i, col).Name, color);
+                }
+                if (row - i >= 0 && down)
+                {
+                    down = CheckFigureColor(cells.Find(row - i, col).Name, color);
+                }
+                if (col + i <= 7 && right)
+                {
+                    right = CheckFigureColor(cells.Find(row, col + i).Name, color);
+                }
+                if (col - i >= 0 && left)
+                {
+                    left = CheckFigureColor(cells.Find(row, col - i).Name, color);
+                }
+            }
+        }
+
+        private void DiagonalMoves(int row, int col, char color)
+        {
+            bool leftUp, leftDown, rightUp, rightDown;
+            leftUp = leftDown = rightUp = rightDown = true;
+            for (int i = 1; i <= 7; i++)
+            {
+                if (row + i <= 7 && col - i >= 0 && leftUp)
+                {
+                    leftUp = CheckFigureColor(cells.Find(row + i, col - i).Name, color);
+                }
+                if (row + i <= 7 && col + i <= 7 && rightUp)
+                {
+                    rightUp = CheckFigureColor(cells.Find(row + i, col + i).Name, color);
+                }
+                if (row - i >= 0 && col + i <= 7 && rightDown)
+                {
+                    rightDown = CheckFigureColor(cells.Find(row - i, col + i).Name, color);
+                }
+                if (row - i >= 0 && col - i >= 0 && leftDown)
+                {
+                    leftDown = CheckFigureColor(cells.Find(row - i, col - i).Name, color);
+                }
+            }
+        }
+
+        private void LightIfNotSameColor(Grid cell, char color)
+        {
+            if (CheckFigureInCell(cell) != color)
+            {
+                LightingCell(cell);
+            }
+        }
+
+        private bool CheckFigureColor(Grid cell, char color)
+        {
+            if (CheckFigureInCell(cell) == 'n')
+            {
+                LightingCell(cell);
+                return true;
+            }
+            if (CheckFigureInCell(cell) != color)
+            {
+                LightingCell(cell);
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private char CheckFigureInCell(Grid cell)
+        {
+            foreach (UIElement item in cell.Children)
+            {
+                if (item.GetValue(FrameworkElement.TagProperty).ToString().Split(' ')[0] == "figure")
+                {
+                    return item.GetValue(FrameworkElement.NameProperty).ToString()[0];
+                }
+            }
+            return 'n';
+        }
+
+        private void LightingCell(Grid cell)
+        {
+            foreach (UIElement item in cell.Children)
+            {
+                if (item.GetValue(FrameworkElement.TagProperty).ToString() == "border")
+                {
+                    item.Visibility = Visibility.Visible;
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    public class Cell
+    {
+        public Cell()
+        {
+
+        }
+
+        public Cell(Grid uIElement, int row, int column)
+        {
+            Name = uIElement;
+            Row = row;
+            Column = column;
+        }
+        public Grid Name { get; }
+        public int Row { get; }
+        public int Column { get; }
+    }
+
+    public class CellsList
+    {
+        public List<Cell> cells = new List<Cell>();
+
+        public void Add(Cell item)
+        {
+            cells.Add(item);
+        }
+
+        public Cell Find(int r, int c)
+        {
+            return cells.Find(x => x.Row == r && x.Column == c);
+        }
+
+        public IEnumerator<Cell> GetEnumerator()
+        {
+            return cells.GetEnumerator();
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public static CellsList cells = new CellsList();
+        private List<UIElement> allFigures = new List<UIElement>();
+        private List<UIElement> allBorders = new List<UIElement>();
+        private UIElement figure;
+        private FiguresAllowedMoves moves = new FiguresAllowedMoves();
+        private int startRow;
+        private int startColumn;
+        private string player = "white";
+        private char selectedFigureColor;
+        public static Thickness thickActive = new Thickness(4, 4, 4, 4);
+        public static Thickness thickDisable = new Thickness(0, 0, 0, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGameField();
+        }
+
+        public void InitializeGameField()
+        {
+            Grid cell;
+            Grid cellForBorder;
+            foreach (UIElement item in grid.Children)
+            {
+                try
+                {
+                    if (item.GetValue(TagProperty).ToString() == "cell")
+                    {
+                        Border border = new Border();
+                        border.BorderBrush = Brushes.ForestGreen;
+                        border.BorderThickness = thickActive;
+                        border.Tag = "border";
+                        border.Visibility = Visibility.Hidden;
+                        cells.Add(new Cell((Grid)item, Grid.GetRow(item), Grid.GetColumn(item)));
+                        cellForBorder = (Grid)item;
+                        cellForBorder.Children.Add(border);
+                        allBorders.Add(border);
+                    }
+
+                    if (item.GetValue(TagProperty).ToString().Split(' ')[0] == "figure")
+                    {
+                        allFigures.Add(item);
+                    }
+                }
+                catch (NullReferenceException)
+                {
+
+                }
+            }
+            foreach (UIElement item in allFigures)
+            {
+                cell = ReturnCell(item);
+                grid.Children.Remove(item);
+                cell.Children.Add(item);
+            }
+        }
+
+        private void Figure_MouseMove(object sender, MouseEventArgs e)
+        {
+            UIElement element = (UIElement)e.Source;
+            lblCursorPosition.Text = $"Column: {startColumn}, Row {startRow}, {element}, {element.GetValue(NameProperty)}," +
+                $" Parent {VisualTreeHelper.GetParent(element).GetValue(NameProperty)}";
+            try
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    DragDropEffects dragDropEffect = DragDrop.DoDragDrop(this, this, DragDropEffects.Move);
+                    if (dragDropEffect == DragDropEffects.None)
+                    {
+                        grid.Children.Remove(figure);
+                        Grid cell = cells.Find(startRow, startColumn).Name;
+                        cell.Children.Add(figure);
+                        Grid.SetRow(figure, startRow);
+                        Grid.SetColumn(figure, startColumn);
+                        BorderDisable();
+                        figure = null;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            UIElement element = (UIElement)e.Source;
+            if (element is Image && player[0] == element.GetValue(NameProperty).ToString()[0])
+            {
+                figure = element;
+                startRow = Grid.GetRow(figure);
+                startColumn = Grid.GetColumn(figure);
+                Grid cell = ReturnCell(figure);
+                selectedFigureColor = figure.GetValue(NameProperty).ToString()[0];
+                Panel.SetZIndex(figure, 2);
+                cell.Children.Remove(figure);
+                grid.Children.Add(figure);
+
+                lblCursorPosition.Text = $"Column: {startColumn}, Row {startRow}, {element}, {element.GetValue(NameProperty)}," +
+                    $" Parent {VisualTreeHelper.GetParent(element).GetValue(NameProperty)}";
+
+                switch (figure.GetValue(TagProperty).ToString().Split(' ')[1])
+                {
+                    case "pawn":
+                        moves.Pawn_Allowed_Moves(startRow, startColumn, selectedFigureColor);
+                        break;
+                    case "king":
+                        moves.King_Allowed_Moves(startRow, startColumn, selectedFigureColor);
+                        break;
+                    case "queen":
+                        moves.Queen_Allowed_Moves(startRow, startColumn, selectedFigureColor);
+                        break;                    
+                    case "bishop":
+                        moves.Bishop_Allowed_Moves(startRow, startColumn, selectedFigureColor);
+                        break;                    
+                    case "knight":
+                        moves.Knight_Allowed_Moves(startRow, startColumn, selectedFigureColor);
+                        break;                  
+                    case "rook":
+                        moves.Rook_Allowed_Moves(startRow, startColumn, selectedFigureColor);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                figure = null;
+            }
+        }
+
+        private void Grid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //Grid cell = cells.Find(startRow, startColumn).Name;
+            if (figure != null)
+            {
+                Grid cell = ReturnCell(figure);
+                //grid.Children.Remove(item);
+                //cell.Children.Add(item);
+                grid.Children.Remove(figure);
+                cell.Children.Add(figure);
+                //Grid.SetRow(figure, startRow);
+                //Grid.SetColumn(figure, startColumn);
+            }
+            BorderDisable();
+            figure = null;
+        }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            var element = (UIElement)e.Source;
+            int lastRow = Grid.GetRow(element);
+            int lastColumn = Grid.GetColumn(element);
+            Grid cell = cells.Find(lastRow, lastColumn).Name;
+            try
+            {
+                if (allBorders.Find(x => VisualTreeHelper.GetParent(x) == cell).Visibility == Visibility.Visible || (lastRow == startRow && lastColumn == startColumn))
+                {
+                    Grid.SetRow(figure, lastRow);
+                    Grid.SetColumn(figure, lastColumn);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Panel.SetZIndex(figure, 1);
+                Grid cell = ReturnCell(figure);
+                //allBorders.Find(x => VisualTreeHelper.GetParent(x) == cell)
+                if (allBorders.Find(x => VisualTreeHelper.GetParent(x) == cell).Visibility == Visibility.Visible)
+                {
+                    grid.Children.Remove(figure);
+                    foreach (UIElement item in cell.Children)
+                    {
+                        if (item is Image)
+                        {
+                            cell.Children.Remove(item);
+                            break;
+                        }
+                    }
+                    cell.Children.Add(figure);
+                    ChangePlayer();
+                }
+                else
+                {
+                    cell = ReturnCell(figure);
+                    grid.Children.Remove(figure);
+                    //cell = cells.Find(startRow, startColumn).Name;
+                    cell.Children.Add(figure);
+                }
+                BorderDisable();
+                figure = null;
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+        }
+
+        private Grid ReturnCell(UIElement element)
+        {
+            int row = Grid.GetRow(element);
+            int column = Grid.GetColumn(element);
+            return cells.Find(row, column).Name;
+        }
+
+        private void BorderDisable()
+        {
+            foreach (Cell item in cells)
+            {
+                foreach (UIElement x in item.Name.Children)
+                {
+                    if (x.GetValue(TagProperty).ToString() == "border")
+                    {
+                        x.Visibility = Visibility.Hidden;
+                    }
+                }
+            }
+        }
+
+        private void ChangePlayer()
+        {
+            player = (player == "white") ? "black" : "white";
+        }
+    }
+}
